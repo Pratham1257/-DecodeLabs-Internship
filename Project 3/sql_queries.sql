60ee6e5aef6535aa0a7ffe72adda14b03c204249
Pratham1257 <pratham.gupta2428@gmail.com>
2026-06-30 23:39:19 +0530

    Add SQL queries for product analysis and order counts in ecommerce_orders

--- Project 3/sql_queries.sql
+++ Project 3/sql_queries.sql
@@ -7,3 +7,16 @@ SELECT * FROM ecommerce_orders LIMIT 5;
 -- Observation:
 -- Each row represents an order and contains customer,
 -- product, payment, quantity and revenue information
+
+SELECT DISTINCT Product FROM ecommerce_orders;
+-- Observation:
+-- The dataset contains 7 unique product categories
+-- that can be used for further sales analysis
+
+SELECT Product, COUNT(*) AS OrderCount
+FROM ecommerce_orders
+GROUP BY Product
+ORDER BY OrderCount DESC;
+-- Observation:
+-- Printer appears most frequently in the dataset,
+-- while Phone has the lowest number of orders
